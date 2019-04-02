746b122dc325805870ffc5d8376f107fff389b98
leopark9497 <46334602+leopark9497@users.noreply.github.com>
2019-04-02 13:27:16 +0300

    Update ex4.js
    Phan Hong Duong

--- students/copy-and-rename/ex4.js
+++ students/copy-and-rename/ex4.js
@@ -1,5 +1,7 @@
 /* 
-ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits. If the function is passed a valid PIN string, return true, else return false. 
+ATM machines allow 4 or 6 digit PIN codes and PIN codes 
+cannot contain anything but exactly 4 digits
+ or exactly 6 digits. If the function is passed a valid PIN string, return true, else return false. 
 
 validatePIN(1234); // false
 validatePIN(00004324); // false
@@ -7,3 +9,6 @@ validatePIN('2312'); // true
 validatePin('asd123'); // false
 validatePin('000010'); // true
  */
+function vilidatePin(input) {
+    return Number.isInteger(input)    &&  input.length===4||6;
+}
